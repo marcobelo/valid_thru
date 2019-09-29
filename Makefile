07b52a19b0c8e83228f67d033a43d628eb501564
barone-dev <marco.barone.belo@gmail.com>
2019-09-29 19:51:11 -0300

    first e2e test, pushing to see if circleci will work

--- Makefile
+++ Makefile
@@ -6,8 +6,11 @@ prepare:
 	python3 -m venv venv
 	. venv/bin/activate && pip install -r requirements/dev.txt
 
-test:
-	. venv/bin/activate && pytest
+test_unit:
+	. venv/bin/activate && pytest tests/unit/
+
+test_e2e:
+	. venv/bin/activate && pytest tests/e2e/
 
 run_server:
 	. venv/bin/activate && python app/app.py
---
+++ tests/e2e/test_app.py
@@ -0,0 +1,17 @@
+import requests
+from hamcrest import assert_that, has_items
+
+BASE_URL = "http://localhost:8000"
+
+
+def test_valid_thru():
+    response = requests.get(f"{BASE_URL}/valid-thru/?month=02&year=2022")
+    data = response.json()
+
+    assert response.status_code == 200
+    assert_that(
+        data[0],
+        has_items(
+            "client_id", "year", "card_holder", "month", "is_active", "card_number"
+        ),
+    )
