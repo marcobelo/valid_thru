060edd6b7122fd8d1d8d3be5d88ee66ceca8203b
barone-dev <marco.barone.belo@gmail.com>
2019-09-29 20:19:57 -0300

    added swagger(only valid-thru endpoint), added insomnia.json configured to run the endpoints

--- tests/e2e/test_app.py
+++ tests/e2e/test_app.py
@@ -1,5 +1,5 @@
 import requests
-from hamcrest import assert_that, has_items
+from hamcrest import assert_that, has_items, has_entries
 
 BASE_URL = "http://localhost:8000"
 
@@ -13,5 +13,18 @@ def test_valid_thru():
         data[0],
         has_items(
             "client_id", "year", "card_holder", "month", "is_active", "card_number"
+        ),
+    )
+
+
+def test_valid_thru_bad_params():
+    response = requests.get(f"{BASE_URL}/valid-thru/?month=xyz&year=abc")
+    data = response.json()
+
+    assert response.status_code == 200
+    assert_that(
+        data,
+        has_entries(
+            {"month": ["Not a valid integer."], "year": ["Not a valid integer."]}
         ),
     )
